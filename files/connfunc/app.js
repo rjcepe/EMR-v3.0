9a1e7975a1938d3ac1d88e6281a1f2e2c4f7cd58
rjcepe <cepenate@gmail.com>
2023-10-25 05:13:13 +0800

    Directory organizing

---
+++ files/connfunc/app.js
@@ -0,0 +1,145 @@
+
+
+//nav functions
+function ToMedRec() {
+  window.location.href = "medicalrecords.html";
+}
+function ToDenRec() {
+  window.location.href = "dentalrecords.html";
+}
+function ToXray() {
+  window.location.href = "xray.html";
+}
+function ToAccRec() {
+  window.location.href = "accidentrec.html";
+}
+function ToConRec() {
+  window.location.href = "consultationrec.html";
+}
+function ToMedCert() {
+  window.location.href = "medcerts.html";
+}
+function ToAccCen() {
+  window.location.href = "accidentcensus.html";
+}
+function logout() {
+  window.location.href = "index.html";
+}
+function ToPresc() {
+  window.location.href = "presc.html";
+}
+
+// supabase connection
+const SUPABASE_URL = "https://yspyqlodogzmrqsifbww.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzcHlxbG9kb2d6bXJxc2lmYnd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgwOTMxNTYsImV4cCI6MjAxMzY2OTE1Nn0.YjQ-8W-UKbg5JPOO0q3aWT2eXjXe593IlxhkZVSAqkk";
+
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+const test = () => {
+    console.log(supabase);
+    console.log("connection is tagumpay, laro ka na muna ulit :)");
+}
+test();
+
+///////////////////////////////////// Load data to table
+async function loadTableData() {
+  const { data: tableData1, error } = await _supabase.from('med_forms1').select("*");
+
+  if (error) {
+    console.log("Error loading table data:", error.message);
+    return;
+  }
+  console.log("hello");
+
+  const tableBody = document.querySelector("#medform_table tbody");
+  // Call the table
+
+  if (tableData1.length === 0) {
+    // If no data in Supabase
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `
+             <th class="row" colspan="7" ">No zata available</td>
+         `;
+    tableBody.appendChild(newRow);
+    console.log("no zata");
+  } else {
+    tableData1.forEach((row) => {
+      const newRow = document.createElement("tr");
+      newRow.classList.add("res");
+
+      newRow.innerHTML = `
+                 <th class="row idcol">${row.patient_id}</th>
+                 <th class="row namecol">${row.patient_name}</th>
+                 <th class="row timecol">${row.date_created}</th>
+                 <th class="row coursecol">${row.course_section}</th>
+                 <th class="row timecol">${row.location}</th>
+                 <th class="row timecol">${row.added_by}</th>
+                 <th class="buttscol"><button class="viewbutt" onclick="showv()"><p class="txt">View</p></button></th>
+                 `;
+      tableBody.appendChild(newRow);
+      console.log("yes zata");
+    });
+  }
+}
+
+loadTableData()
+
+
+///////////////////////////////////// insert student medform data to table
+$('#form1').submit(async function (event) {
+  event.preventDefault();
+
+  // Get form field values
+  const name = $('#name').val();
+
+  const medformInput = document.getElementById('medform');
+  const medformFile = medformInput.files[0];
+
+  // Perform the insert operation with both name and medform
+  try {
+      const { data: existingData, error } = await _supabase.from('table_name').select('*').eq('name', name);
+
+      if (error) {
+          console.log("errorrr", error.message);
+          return;
+      }
+
+      if (existingData.length > 1) {
+          console.log("Already Existing", existingData);
+          return;
+      }
+
+      // Change the filename to "(name inputted)_medform"
+      const fileName = `${name}_medform.${medformFile.name.split('.').pop()}`;
+      
+      // Upload the file to Supabase storage with the modified filename
+      const { data, error: uploadError } = await _supabase.storage.from('records').upload(fileName, medformFile);
+
+      if (uploadError) {
+          console.error('Error uploading file:', uploadError);
+          return;
+      }
+      
+      const medformURL = SUPABASE_URL + "/storage/v1/object/public/records/"+ fileName; //improvised link
+      
+  
+      const medformInfo = {
+          name: name,
+          med_form: medformURL,
+      };
+
+      // Insert data into Supabase table
+      const { data: insertData, error: insertError } = await _supabase.from('table_name').insert(medformInfo);
+
+      if (insertError) {
+          console.error('Error inserting data:', insertError.message);
+      } else {
+          console.log('Data inserted successfully:', insertData);
+          location.reload();
+      }
+
+  } catch (error) {
+      console.error('Error inserting data:', error.message);
+  }
+});
