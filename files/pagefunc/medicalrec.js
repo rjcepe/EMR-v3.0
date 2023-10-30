3fa6d6324a28cfa3324f7d9e96548fdd2f5dcf25
rjcepe <cepenate@gmail.com>
2023-10-30 08:24:59 +0800

    Update medicalrec.js

--- files/pagefunc/medicalrec.js
+++ files/pagefunc/medicalrec.js
@@ -6,68 +6,57 @@ const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 ///////////////////////////////////// Load data to table
 //////////////////////////////// sort function
-// Add an event listener to the select element to detect changes
-loadTableData();
+  // Add an event listener to the select element to detect changes
+  loadTableData();
 
-document.getElementById("sort1").addEventListener("change", function () {
-  loadTableData(); // Reload the table data when the sorting option changes
-});
-
-// Modify the loadTableData function to sort the table data
-async function loadTableData() {
-  const selectedOption = document.getElementById("sort1").value;
-
-  const { data: tableData1, error } = await _supabase
-    .from("med_forms")
-    .select("*");
-
-  if (error) {
-    console.log("Error loading table data:", error.message);
-    return;
-  }
-
-  const tableBody = document.querySelector("#medform_table tbody");
-  tableBody.innerHTML = ""; // Clear the current table
-
-  if (tableData1.length === 0) {
-    const newRow = document.createElement("tr");
-    newRow.innerHTML = `<th class="row" colspan="7">No data available</td>`;
-    tableBody.appendChild(newRow);
-    console.log("No data");
-  } else {
-    // Sort the data based on the selected option
-    if (selectedOption === "ID") {
-      tableData1.sort(
-        (a, b) => new Date(a.patient_id) - new Date(b.patient_id)
-      );
-    } else if (selectedOption === "TimeLate") {
-      tableData1.sort((a, b) => new Date(b.row_id) - new Date(a.row_id));
-    } else if (selectedOption === "TimeOld") {
-      tableData1.sort((a, b) => new Date(a.row_id) - new Date(b.row_id));
-    } else if (selectedOption === "def") {
-      tableData1.sort((a, b) => new Date(a.row_id) - new Date(b.row_id));
-    } else if (selectedOption === "Nameaz") {
-      tableData1.sort((a, b) =>
-        a.patient_name.toString().localeCompare(b.patient_name)
-      );
-    } else if (selectedOption === "Nameza") {
-      tableData1.sort((a, b) =>
-        b.patient_name.toString().localeCompare(a.patient_name)
-      );
-    } else if (selectedOption === "CS") {
-      tableData1.sort((a, b) =>
-        a.course_section.toString().localeCompare(b.course_section)
-      );
-    } else if (selectedOption === "EMP") {
-      tableData1.sort((a, b) =>
-        b.course_section.toString().localeCompare(a.course_section)
-      );
+  document.getElementById("sort1").addEventListener("change", function () {
+    loadTableData(); // Reload the table data when the sorting option changes
+  });
+  
+  // Modify the loadTableData function to sort the table data
+  async function loadTableData() {
+    const selectedOption = document.getElementById("sort1").value;
+  
+    const { data: tableData1, error } = await _supabase.from("med_forms").select("*");
+  
+    if (error) {
+      console.log("Error loading table data:", error.message);
+      return;
     }
-
-    tableData1.forEach((row) => {
+  
+    const tableBody = document.querySelector("#medform_table tbody");
+    tableBody.innerHTML = ""; // Clear the current table
+  
+    if (tableData1.length === 0) {
       const newRow = document.createElement("tr");
-      newRow.classList.add("res");
-      newRow.innerHTML = `
+      newRow.innerHTML = `<th class="row" colspan="7">No data available</td>`;
+      tableBody.appendChild(newRow);
+      console.log("No data");
+    } else {
+      // Sort the data based on the selected option
+      if (selectedOption === "ID") {
+        tableData1.sort((a, b) => new Date(a.patient_id) - new Date(b.patient_id));
+      } else if (selectedOption === "TimeLate") {
+        tableData1.sort((a, b) => new Date(b.row_id) - new Date(a.row_id));
+      } else if (selectedOption === "TimeOld") {
+        tableData1.sort((a, b) => new Date(a.row_id) - new Date(b.row_id));
+      } else if (selectedOption === "def") {
+        tableData1.sort((a, b) => new Date(a.row_id) - new Date(b.row_id));
+      } else if (selectedOption === "Nameaz") {
+        tableData1.sort((a, b) => a.patient_name.toString().localeCompare(b.patient_name));
+      } else if (selectedOption === "Nameza") {
+        tableData1.sort((a, b) => b.patient_name.toString().localeCompare(a.patient_name));
+      } else if (selectedOption === "CS") {
+        tableData1.sort((a, b) => a.course_section.toString().localeCompare(b.course_section));
+      } else if (selectedOption === "EMP") {
+        tableData1.sort((a, b) => b.course_section.toString().localeCompare(a.course_section));
+      }
+      
+  
+      tableData1.forEach((row) => {
+        const newRow = document.createElement("tr");
+        newRow.classList.add("res");
+        newRow.innerHTML = `
           <th class="row idcol">${row.patient_id}</th>
           <th class="row namecol">${row.patient_name}</th>
           <th class="row timecol">${row.created_date}</th>
@@ -80,13 +69,14 @@ async function loadTableData() {
             </button>
           </th>
         `;
-      tableBody.appendChild(newRow);
-    });
+        tableBody.appendChild(newRow);
+      });
+    }
   }
-}
+
 
 ////////////////////////////// fetch username
-async function getusername() {
+ async function getusername(){
   var id1 = localStorage.getItem("uid1");
 
   const { data, error } = await _supabase
@@ -102,7 +92,7 @@ async function getusername() {
   // Check if data is not empty
   if (data && data.length > 0) {
     const username = data[0].username;
-
+    
     getusername1(username);
 
     // Now, you can use the 'username' variable as needed.
@@ -111,12 +101,14 @@ async function getusername() {
   }
 }
 
-function getusername1(username) {
+function getusername1(username){
+
   var bb = username;
   localStorage.setItem("x", bb);
 }
 
 getusername();
+
 
 $("#insertstudmedform").submit(async function (event) {
   event.preventDefault();
@@ -152,9 +144,7 @@ $("#insertstudmedform").submit(async function (event) {
   // Initialize the 'user' variable outside the try-catch block
   try {
     // Change the filename to "(name inputted)_medform"
-    const fileName = `${id}_medform${studmedfilecount}.${medformFile.name
-      .split(".")
-      .pop()}`;
+    const fileName = `${id}_medform${studmedfilecount}.${medformFile.name.split(".").pop()}`;
 
     // Upload the file to Supabase storage with the modified filename
     const { data, error: uploadError } = await _supabase.storage
@@ -176,15 +166,10 @@ $("#insertstudmedform").submit(async function (event) {
 
     // Get the current date and time in the target timezone
     const today = new Date();
-    const options = {
-      timeZone: targetTimezone,
-      year: "numeric",
-      month: "numeric",
-      day: "numeric",
-    };
+    const options = { timeZone: targetTimezone, year: 'numeric', month: 'numeric', day: 'numeric' };
     const date1 = today.toLocaleString("en-US", options);
-    const dateParts = date1.split("/"); // Split the date by slashes
-    const dateWithDashes = dateParts.join("-"); // Join the date parts with dashes
+    const dateParts = date1.split('/'); // Split the date by slashes
+    const dateWithDashes = dateParts.join('-'); // Join the date parts with dashes
 
     const CurrentDate = dateWithDashes.toString();
 
@@ -214,9 +199,10 @@ $("#insertstudmedform").submit(async function (event) {
   }
 });
 
+
 $("#insertempmedform").submit(async function (event) {
   event.preventDefault();
-
+  
   // Retrieve the current empmedfilecount value from local storage
   let empmedfilecount = localStorage.getItem("empmedfilecount");
 
@@ -247,9 +233,7 @@ $("#insertempmedform").submit(async function (event) {
   // Initialize the 'user' variable outside the try-catch block
   try {
     // Change the filename to "(name inputted)_medform"
-    const fileName = `${id1}_medform${empmedfilecount}.${medformFile.name
-      .split(".")
-      .pop()}`;
+    const fileName = `${id1}_medform${empmedfilecount}.${medformFile.name.split(".").pop()}`;
 
     // Upload the file to Supabase storage with the modified filename
     const { data, error: uploadError } = await _supabase.storage
@@ -271,15 +255,10 @@ $("#insertempmedform").submit(async function (event) {
 
     // Get the current date and time in the target timezone
     const today = new Date();
-    const options = {
-      timeZone: targetTimezone,
-      year: "numeric",
-      month: "numeric",
-      day: "numeric",
-    };
+    const options = { timeZone: targetTimezone, year: 'numeric', month: 'numeric', day: 'numeric' };
     const date1 = today.toLocaleString("en-US", options);
-    const dateParts = date1.split("/"); // Split the date by slashes
-    const dateWithDashes = dateParts.join("-"); // Join the date parts with dashes
+    const dateParts = date1.split('/'); // Split the date by slashes
+    const dateWithDashes = dateParts.join('-'); // Join the date parts with dashes
 
     const CurrentDate = dateWithDashes.toString();
 
@@ -309,14 +288,12 @@ $("#insertempmedform").submit(async function (event) {
   }
 });
 
+
 //////////////////////////////////// user display
 async function fetchUsername() {
   var id1 = localStorage.getItem("uid1");
 
-  const { data, error } = await _supabase
-    .from("user_accs")
-    .select("username")
-    .eq("id", id1);
+  const { data, error } = await _supabase.from("user_accs").select("username").eq("id", id1);
 
   if (error) {
     console.error("Error fetching username:", error.message);
@@ -347,25 +324,27 @@ async function fetchUsername() {
   } else {
     console.log("User not found with ID:", id);
   }
+
 }
 
-////////// display corresponding user pic
+////////// display corresponding user pic  
 async function fetchUserPic() {
   var id1 = localStorage.getItem("uid1");
-  const piclink = id1 + ".png";
+  const piclink = id1 + '.png';
   console.log(piclink);
 
   const userpiclink = `${SUPABASE_URL}/storage/v1/object/public/userimages/${piclink}`;
 
   const userTab = document.querySelector(".user");
-  const usernameDiv = document.querySelector(".username");
+    const usernameDiv = document.querySelector(".username");
 
-  console.log(userpiclink);
+    console.log(userpiclink);
 
-  const img = document.createElement("img");
-  img.setAttribute("src", userpiclink);
+    const img = document.createElement('img');
+    img.setAttribute("src", userpiclink);
 
-  userTab.insertBefore(img, usernameDiv);
+    userTab.insertBefore(img, usernameDiv);
+
 }
 
 fetchUserPic();
@@ -408,45 +387,41 @@ function hidev() {
   // Check if filec and name1 are defined before removing them
   if (filec && name1) {
     setTimeout(() => {
-      file.removeChild(filec);
-      file.removeChild(name1);
-    }, 500); // .5s delay time
-  }
+        file.removeChild(filec);
+        file.removeChild(name1);
+      }, 500); // .5s delay time 
+    }
 
   main.classList.remove("main-filter");
   vfile.classList.add("hidev");
   vfile.classList.remove("showv");
 }
 
+
 //////////////////////////////////search function
-document
-  .getElementById("searchpatient")
-  .addEventListener("click", async function () {
+document.getElementById("searchpatient").addEventListener("click", async function() {
     // Get the patient ID entered in the input field
     const searchID = document.getElementById("searchInput").value;
-
+  
     // Fetch the patient data based on the search ID
-    const { data: patientData, error } = await _supabase
-      .from("med_forms")
-      .select("*")
-      .eq("patient_id", searchID);
-
+    const { data: patientData, error } = await _supabase.from("med_forms").select("*").eq("patient_id", searchID);
+  
     if (error) {
       console.error("Error fetching patient data:", error.message);
       return;
     }
-
+  
     const tableBody = document.querySelector("#medform_table tbody");
-
+  
     // Clear the current table
     tableBody.innerHTML = "";
-
+  
     if (patientData && patientData.length > 0) {
       // Patient data found, update the table
       patientData.forEach((row) => {
         const newRow = document.createElement("tr");
         newRow.classList.add("res");
-
+  
         newRow.innerHTML = `
           <th class="row idcol">${row.patient_id}</th>
           <th class="row namecol">${row.patient_name}</th>
@@ -460,7 +435,7 @@ document
             </button>
           </th>
         `;
-
+  
         tableBody.appendChild(newRow);
       });
     } else {
@@ -472,3 +447,8 @@ document
       tableBody.appendChild(newRow);
     }
   });
+
+
+
+
+  
