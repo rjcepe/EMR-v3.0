5e0c5eeba02ff2e53d6e8764e792129ff925cf74
rjcepe <cepenate@gmail.com>
2023-10-30 14:38:01 +0800

    fixed

--- files/pagefunc/consrec.js
+++ files/pagefunc/consrec.js
@@ -237,7 +237,7 @@ $("#insertempconsform").submit(async function (event) {
       console.error("Error inserting data:", insertError.message);
     } else {
       console.log("Data inserted successfully:", insertData);
-    //   location.reload();
+      location.reload();
     }
   } catch (error) {
     console.error("Error:", error.message);
