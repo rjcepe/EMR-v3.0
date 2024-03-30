0e8f43d9ce5be5e5ec36601e7bc50dbd99c14c7f
rjcepe <118280950+rjcepe@users.noreply.github.com>
2024-03-30 11:28:57 +0800

    Delete files/connfunc/restrictMobile.js

--- files/connfunc/restrictMobile.js
+++
@@ -1,9 +0,0 @@
-const screenWidth = window.innerWidth;
-const screenHeight = window.innerHeight;
-
-console.log(screenHeight);
-console.log(screenWidth);
-
-if (screenWidth < 500) {
-    NoMobile();
-}
